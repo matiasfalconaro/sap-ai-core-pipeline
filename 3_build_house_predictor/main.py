1defa15eedd8f8d15eb847370b65c4f1682928a2
mfalconaro <matiasfalconaro@gmail.com>
2024-06-20 10:01:44 -0300

    Rearrange main

--- 3_build_house_predictor/main.py
+++ 3_build_house_predictor/main.py
@@ -1,10 +1,10 @@
 #!usr/bin/python3
 
-from sklearn import datasets # Load Datasets
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 
-data_house = datasets.fetch_california_housing()
+data_house = datasets.fetch_california_housing() # Load Datasets
 X = data_house['data']
 y = data_house['target']
 
@@ -12,15 +12,13 @@ y = data_house['target']
 train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
 
 clf = DecisionTreeRegressor() # Init model
-
 clf.fit(train_x, train_y) # Train model
-
 test_r2_score = clf.score(test_x, test_y) # Test model
-# Output will be available in logs of SAP AI Core.
+
+print(f"Test Data Score {test_r2_score}")
 
 '''
+Output will be available in logs of SAP AI Core.
 Not the ideal way of storing /reporting metrics in SAP AI Core,
 but that is not the focus this tutorial
 '''
-
-print(f"Test Data Score {test_r2_score}")
