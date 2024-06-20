befe9f2a902fcf30e555b1fdd145d93f460e4cbd
mfalconaro <matiasfalconaro@gmail.com>
2024-06-20 09:53:26 -0300

    PEP8 compliance

--- 3_build_house_predictor/main.py
+++ 3_build_house_predictor/main.py
@@ -1,25 +1,26 @@
 #!usr/bin/python3
 
+from sklearn import datasets # Load Datasets
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 
-# Load Datasets
-from sklearn import datasets
 data_house = datasets.fetch_california_housing()
 X = data_house['data']
 y = data_house['target']
 
 # Partition into Train and test dataset
-from sklearn.model_selection import train_test_split
 train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
 
-# Init model
-from sklearn.tree import DecisionTreeRegressor
-clf = DecisionTreeRegressor()
+clf = DecisionTreeRegressor() # Init model
 
-# Train model
-clf.fit(train_x, train_y)
+clf.fit(train_x, train_y) # Train model
 
-# Test model
-test_r2_score = clf.score(test_x, test_y)
+test_r2_score = clf.score(test_x, test_y) # Test model
 # Output will be available in logs of SAP AI Core.
-# Not the ideal way of storing /reporting metrics in SAP AI Core, but that is not the focus this tutorial
+
+'''
+Not the ideal way of storing /reporting metrics in SAP AI Core,
+but that is not the focus this tutorial
+'''
+
 print(f"Test Data Score {test_r2_score}")
